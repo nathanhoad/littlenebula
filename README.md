45bd84330ae5e2f8fcead0f05915222d1c120433
Nathan Hoad <nathan@nathanhoad.net>
2016-03-15 16:45:30 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Little Nebula
+
+The code that powers [littlenebula.com](https://littlenebula.com).
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+
+
+get '/' do
+  erb :index
+end
